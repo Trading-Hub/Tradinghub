9760634a7ede0be711496f7c2b18a156f6f86247
Arth <info.tradinghub1@gmail.com>
2021-07-28 22:44:07 +0530

    test deploy

--- Tradinghub/urls.py
+++ Tradinghub/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.static import serve
+from django.conf.urls import url
 
 admin.site.site_header = " TradingHub Admin"
 admin.site.site_title = "TradingHub Site Admin"
@@ -22,4 +24,6 @@ admin.site.site_title = "TradingHub Site Admin"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('polls.url'))
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
