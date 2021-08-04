9d8319c9e173b4b9c88399733477299a5649aa5b
Arth <info.tradinghub1@gmail.com>
2021-08-04 09:42:41 +0530

    heroku test2

--- Tradinghub/settings.py
+++ Tradinghub/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*(az&rcb5aa(#(zb^$r*cs5#f-gkpi10gboifuhm_k6s4y9$$e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','https://tradinghub.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
